4f1cf7b7f8cd73dae4e64e63e77ebbffb798fc0e
Eric Dayle Loon <EDLoon@CSSERVER.local>
2024-09-18 14:40:24 +0800

    Layout files

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
