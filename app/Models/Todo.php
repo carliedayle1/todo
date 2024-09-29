7232b6e0fca048fd34b3617c55ae9652ccf5e851
m4yoNICE <cenizaroswellrey@gmail.com>
2024-09-29 22:59:09 +0800

    Activity 2: added migration and model

---
+++ app/Models/Todo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'todos';
+    protected $fillable = ['title', 'description', 'completed'];
+}
