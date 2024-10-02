96abf925e7b1f841d4682f148df16ad23d542a9e
dacereal <dapanascaryl@gmail.com>
2024-10-02 12:24:25 +0800

    Activity 1- Layout
    Activity 1- Layout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        About page
+    </x-slot:heading>
+<h1> About Page </h1>
+</x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,7 +1,6 @@
 <x-layout>
     <x-slot:heading>
-        Home page
+        Contact page
     </x-slot:heading>
-    
-    <h1> Home Page </h1>
+<h1> Contact Page </h1>
 </x-layout>
