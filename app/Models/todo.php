a791cdc3b39c623c286dd595d6ce1889b1912b77
wukinnin <nimugonzaga@protonmail.com>
2024-10-06 07:50:53 +0800

    activity 2 - models and migrations

---
+++ app/Models/todo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class todo extends Model
+{
+    use HasFactory;
+
+    protected $table = 'todos';
+    protected $fillable = ['title', 'description', 'completed'];
+}
