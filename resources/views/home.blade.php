7431875120624be39769367b1598632dce4a5310
jorskie2003 <markjorlandiway1234@gmail.com>
2024-10-06 12:19:08 +0800

    Activity 5 - Display todo in a table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,25 @@
     <x-slot:heading>
         Home Page
     </x-slot:heading>
-<h1>Hello from the home page</h1>
-
+    <h1>Hello from the home page</h1>
+    <table class="table-auto">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Completed</th>
+            </tr>
+        </thead>
+        <body>
+        @foreach($todos as $todo)
+            <tr>
+                <td>{{ $todo->id }}</td> 
+                <td>{{ $todo->title }}</td>
+                <td>{{ $todo->description }}</td>
+                <td>{{ $todo->completed ? 'Yes' : 'No' }}</td>                  
+            </tr>
+        @endforeach
+        </body>
+    </table>
 </x-layout>
