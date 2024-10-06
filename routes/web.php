b27b6c987fff8f0f5cfb3699b83860ad43eb6fbf
Scythelicia <147374209+Scythelicia@users.noreply.github.com>
2024-10-07 01:19:09 +0800

    Activity 3 - Controller

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function () {
+
+
+Route::get('/about', function () {
     return view('about');
 });
-Route::get('/', function () {
+
+
+Route::get('/contact', function () {
     return view('contact');
 });
+
+// Resource route for todos
+Route::resource('todos', TodoController::class);
