f1f57a4857a307f61c267d80ab166c897f92fb15
Megan Ys B. Tepait <meganclan2017@gmail.com>
2024-10-08 11:59:41 +0800

    ACTIVITY 3 CONTROLLER

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/', [TodoController::class, 'welcome']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/about', [TodoController::class, 'about']);
+
+Route::get('/contact', [TodoController::class, 'contact']);
