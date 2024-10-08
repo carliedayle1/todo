2f06b12c2b6e05e6b92ccdfdf37332f8583194a6
halimawmagselos <maykylacabanog@gmail.com>
2024-10-08 22:13:49 +0800

    Activity 4 - Seeders and Factory (revised)

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Todo;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       // Todo::factory(10)->create();
+
+        Todo::factory()->create([
+            'title' => fake()->title(),
+            'description' => fake()->paragraph(),
+        ]);
+    }
+}
