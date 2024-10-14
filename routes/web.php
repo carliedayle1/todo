ed91c4fd6241cd949191028fd4735b0e326d03c7
Jarcel Franz Tubigon <jarcelfranz@gmail.com>
2024-10-14 23:25:35 +0800

    Activity 3- Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,4 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+Route::resource('todos', TodoController::class);
