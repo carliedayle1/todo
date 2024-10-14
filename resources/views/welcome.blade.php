3f0ec906ca2599cf909bb822398e69678b44f0df
DESKTOP-QVORR98\james <pogi.sherlocked@gmail.com>
2024-10-14 23:44:05 +0800

    Activity 5 Display with tables

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,30 +2,45 @@
     <x-slot:heading>
         Welcome
     </x-slot>
-    <table class="table-auto">
-  <thead>
-    <tr>
-      <th>Song</th>
-      <th>Artist</th>
-      <th>Year</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-      <td>Malcolm Lockyer</td>
-      <td>1961</td>
-    </tr>
-    <tr>
-      <td>Witchy Woman</td>
-      <td>The Eagles</td>
-      <td>1972</td>
-    </tr>
-    <tr>
-      <td>Shining Star</td>
-      <td>Earth, Wind, and Fire</td>
-      <td>1975</td>
-    </tr>
-  </tbody>
-</table>
+    
+
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    ID
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Description
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Completed
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($todos as $todo)
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {{ $todo->id }}
+                </th>
+                <td class="px-6 py-4">
+                    {{ $todo->name }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $todo->description }}
+                </td>
+                <td class="px-6 py-4">
+                    {{ $todo->completed ? 'Yes' : 'No' }}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
 </x-layout>
