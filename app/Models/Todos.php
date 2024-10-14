dc59358bc768899c7b98ff8b226ffc829c03e396
DESKTOP-QVORR98\james <pogi.sherlocked@gmail.com>
2024-10-14 22:23:04 +0800

    Activity 2 Migration and Models

---
+++ app/Models/Todos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todos extends Model
+{
+    use HasFactory;
+
+    protected $table = 'Todos';
+    protected $fillable = ['name', 'description', 'completed'];
+}
