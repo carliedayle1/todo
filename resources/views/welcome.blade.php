b00722eca1de9438404f19f6e85264e84f7562bf
John Clarence Belarmino <clarencebelarmino@gmail.com>
2024-10-14 23:08:04 +0800

    Activity 1 - Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -60,6 +60,7 @@
                 </div>
 
                 <div class="flex items-center justify-center">
+                  
                     <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-teal-500 text-white hover:bg-teal-600 focus:outline-none focus:bg-teal-600 disabled:opacity-50 disabled:pointer-events-none">
                         Submit
                       </button>
