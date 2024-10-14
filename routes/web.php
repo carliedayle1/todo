be64df9f1e37a74233d1486a905f89e6684ab63e
unknown <PI325@csserver.local>
2024-10-14 14:11:51 +0800

    Route update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TodosController::class, 'home']);
+
+Route::get('/about', [TodosController::class, 'about']);
+
+Route::get('/contact', [TodosController::class, 'contacts']);
