52e738d97342734d1d7e2ccf6f341f0a9d2e66f0
unknown <Admin@CSLAB209.CSSERVER.local>
2024-10-14 14:45:07 +0800

    Create Todo.php

---
+++ app/Models/Todo.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Todo extends Model
+{
+    use HasFactory;
+    // Fillable fields
+    protected $fillable = ['title', 'description', 'completed'];
+}
