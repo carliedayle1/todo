f637b356d2bf70bef4ddac9ea02f94c807f5a642
Megan Ys B. Tepait <meganclan2017@gmail.com>
2024-10-14 23:58:29 +0800

    ACTIVITY 5 DISPLAY WITH TABLES

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
     public function welcome()
     {
-        return view('welcome');
+        $todos = Todo::all();
+       //dd($todos);
+       return view('welcome', [
+        'todos' => $todos,
+    ]);
     }
 
     public function about()
