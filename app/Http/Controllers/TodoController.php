43c931200eaca2a6aee6205cb5de1fe911ebd42d
MarteDan <aresdmarte@gmail.com>
2024-10-15 05:40:01 +0800

    Activity 3 - Controllers

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function home()
+    {
+        $todos = Todo::all();
+
+        
+        return view('home', [
+            'todos' => $todos,
+        ]);
+    } 
+    public function welcome()
+    {
+        // Return the welcome view
+        return view('welcome');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+}
