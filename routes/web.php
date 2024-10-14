f75b7dc3af09816dcae0ae6638d72b638dc88075
sanoria <akashikun14@yahoo.com>
2024-10-15 00:22:51 +0800

    Activity 3 - Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [TodoController::class, 'welcome']);
+
+Route::get('/about', [TodoController::class, 'about']);
+
+Route::get('/contact', [TodoController::class, 'contact']);
