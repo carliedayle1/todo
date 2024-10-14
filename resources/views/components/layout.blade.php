33db14397140df3320bc74e99076f4158653e806
unknown <PI305@csserver.local>
2024-10-14 16:39:12 +0800

    Activity - 1 Layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Todo App</title>
+    <title>Todo todo</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
