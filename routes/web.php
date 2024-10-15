d6399ba621c7ad0af9588007a87eb13e248d7977
Christy <christy221mae@gmail.com>
2024-10-15 15:29:13 +0800

    Activity 7 - Forms and Tables

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', [TodoController::class, 'welcome']);
 Route::get('/about', [TodoController::class, 'about']);
 
 Route::get('/contact', [TodoController::class, 'contact']);
+
+Route::get('create', [TodoController::class, 'create']);
