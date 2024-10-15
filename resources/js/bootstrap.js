8056b5ff26481084fa49b32200104b8ed23c9f19
Christy <christy221mae@gmail.com>
2024-10-15 15:26:02 +0800

    Activity 6 - New Layout

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,25 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// This code should be added to <head>.
+// It's used to prevent page load glitches.
+const html = document.querySelector("html");
+const isLightOrAuto =
+    localStorage.getItem("hs_theme") === "light" ||
+    (localStorage.getItem("hs_theme") === "auto" &&
+        !window.matchMedia("(prefers-color-scheme: dark)").matches);
+const isDarkOrAuto =
+    localStorage.getItem("hs_theme") === "dark" ||
+    (localStorage.getItem("hs_theme") === "auto" &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches);
+
+if (isLightOrAuto && html.classList.contains("dark"))
+    html.classList.remove("dark");
+else if (isDarkOrAuto && html.classList.contains("light"))
+    html.classList.remove("light");
+else if (isDarkOrAuto && !html.classList.contains("dark"))
+    html.classList.add("dark");
+else if (isLightOrAuto && !html.classList.contains("light"))
+    html.classList.add("light");
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
-    <x-slot:heading>
-        Contact
-    </x-slot>
- <h1>Contact Page</h1>
+    <div class="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
+        <h1 class="text-2xl font-semibold mb-4">Contact Page</h1>
+    </div>
 </x-layout>
