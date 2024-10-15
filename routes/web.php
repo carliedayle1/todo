063131510f55ab558c90d8ff9ff9ed8092db4099
DESKTOP-USQG1SP\Ivan Cuyos <cuyosivan1@gmail.com>
2024-10-15 05:02:04 -0700

    Activity 3: Controller

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/about', function () {
-    return view('about');
-});
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/', [TodoController::class, 'welcome']);
+
+Route::get('/about', [TodoController::class, 'about']);
+
+Route::get('/contact', [TodoController::class, 'contact']);
