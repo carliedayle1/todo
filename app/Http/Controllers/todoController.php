bcf41fddc5743de0ccb96c2a7ef11b2fb9abd092
DESKTOP-USQG1SP\Ivan Cuyos <cuyosivan1@gmail.com>
2024-10-15 12:16:04 -0700

    Activity 5: Display with tables

---
+++ app/Http/Controllers/todoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function welcome()
+    {
+        return view('welcome');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+        
+}
