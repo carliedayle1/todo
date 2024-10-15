cf62b07211574da532af80c84d94090376e8d3d3
unknown <caspertesado@gmail.com>
2024-10-15 13:58:55 +0800

    Act 5

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,18 @@
-<?php
-
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
 {
-    //
-}
+    public function welcome()
+    {
+        return view('welcome');
+        $todos = Todo::all();
+       //dd($todos);
+       return view('welcome', [
+        'todos' => $todos,
+    ]);
+    }
+
+    public function about()
