9568b4e8dd3cd3afd5e87ff7b3624a2161de3d95
Rembrant Pasardan <160525899+BrantySis@users.noreply.github.com>
2024-10-15 22:14:00 +0800

    Activity 4 Seeders and Factories

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Todos extends Model
+class Todo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'completed'];
 }
