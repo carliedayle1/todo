e68ae71837a7b09b8a372c50640ddef31af60f16
StickyMucus <frixadrian2@gmail.com>
2024-10-15 10:58:01 +0800

    ACTIVITY 3 CONTROLLER

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class TodoController extends Controller
+{
+    public function welcome()
+    {
+        return view('welcome');
+    }
+    public function about()
+    {
+        return view('about');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+        
+}
