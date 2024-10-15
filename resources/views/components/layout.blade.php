91753fccabed62e7a9ac284a1bd92f76e7ae536a
Rembrant Pasardan <160525899+BrantySis@users.noreply.github.com>
2024-10-15 22:51:53 +0800

    Activity 6 new layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,6 +11,9 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-3gW7w9dE3JGH4F0dh/R9A5+3Rt8ERcZacGNHlD+/3THvN4l/c5ZGHF4qgOhAehIn" crossorigin="anonymous">
+
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -25,6 +28,11 @@
     </main>
 
     <x-footer />
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha384-J6qa4849BlE+YO9i3J6C8Gm3XQ8lRm8a5J9T+NIN2XZp0gP4VWX5eyZR46xdG33p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-Ogvrx1sX1ZV7I4dq1JzXeI+Vq7D5HByi+3ZMXZgRgFbC4Uo09C5T7VEE5F3WOPW7" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js" integrity="sha384-s7CP5V2ZZXJ23ckD4X4u3kZclFaHxOhwKx2h1fQQ72M/+s5n7+y1J5p45P6fp81t" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,9 @@
 <!-- ========== HEADER ========== -->
 <header class="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full py-7 bg-white dark:bg-gray-800 shadow-md">
-    <nav class="relative max-w-7xl w-full flex flex-wrap md:grid md:grid-cols-12 items-center px-4 md:px-6 md:px-8 mx-auto">
+    <nav class="navbar navbar-expand-md navbar-light bg-light relative max-w-7xl w-full flex flex-wrap md:grid md:grid-cols-12 items-center px-4 md:px-6 md:px-8 mx-auto">
         <div class="md:col-span-3">
             <!-- Logo -->
-            <a class="flex-none rounded-xl text-xl inline-block font-semibold text-gray-800 dark:text-white focus:outline-none focus:opacity-80" href="/" aria-label="Todo">
+            <a class="navbar-brand rounded-xl text-xl font-semibold text-gray-800 dark:text-white" href="/" aria-label="Todo">
                 Todo
             </a>
             <!-- End Logo -->
@@ -11,14 +11,14 @@
 
         <!-- Button Group -->
         <div class="flex items-center gap-x-1 md:gap-x-2 ms-auto py-1 md:ps-6 md:order-3 md:col-span-3">
-            <button type="button" class="hs-dark-mode-active:hidden block hs-dark-mode font-medium text-gray-800 rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" data-hs-theme-click-value="dark">
+            <button type="button" class="btn btn-light rounded-circle" data-hs-theme-click-value="dark">
                 <span class="group inline-flex shrink-0 justify-center items-center size-9">
                     <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
                     </svg>
                 </span>
             </button>
-            <button type="button" class="hs-dark-mode-active:block hidden hs-dark-mode font-medium text-gray-800 rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:text-neutral-200 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800" data-hs-theme-click-value="light">
+            <button type="button" class="btn btn-light rounded-circle" data-hs-theme-click-value="light">
                 <span class="group inline-flex shrink-0 justify-center items-center size-9">
                     <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <circle cx="12" cy="12" r="4"></circle>
@@ -35,7 +35,7 @@
             </button>
 
             <div class="md:hidden">
-                <button type="button" class="hs-collapse-toggle size-[38px] flex justify-center items-center text-sm font-semibold rounded-xl border border-gray-200 text-black hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" id="hs-navbar-hcail-collapse" aria-expanded="false" aria-controls="hs-navbar-hcail" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-hcail">
+                <button type="button" class="btn btn-outline-secondary" id="hs-navbar-hcail-collapse" aria-expanded="false" aria-controls="hs-navbar-hcail" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-hcail">
                     <svg class="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6"/><line x1="3" x2="21" y1="12" y2="12"/><line x1="3" x2="21" y1="18" y2="18"/></svg>
                     <svg class="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
                 </button>
@@ -45,10 +45,10 @@
 
         <!-- Collapse -->
         <div id="hs-navbar-hcail" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block md:w-auto md:basis-auto md:order-2 md:col-span-6" aria-labelledby="hs-navbar-hcail-collapse">
-            <div class="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
-                <x-navlink name="home" class="hover:text-teal-500 transition duration-300">Home</x-navlink>
-                <x-navlink name="about" class="hover:text-teal-500 transition duration-300">About</x-navlink>
-                <x-navlink name="contact" class="hover:text-teal-500 transition duration-300">Contact</x-navlink>
+            <div class="navbar-nav flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:justify-center md:items-center md:gap-y-0 md:gap-x-7 md:mt-0">
+                <x-navlink name="home" class="nav-link hover:text-teal-500 transition duration-300">Home</x-navlink>
+                <x-navlink name="about" class="nav-link hover:text-teal-500 transition duration-300">About</x-navlink>
+                <x-navlink name="contact" class="nav-link hover:text-teal-500 transition duration-300">Contact</x-navlink>
             </div>
         </div>
         <!-- End Collapse -->
