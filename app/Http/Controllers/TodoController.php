f05bfc878dd7cc311f8865f700638a33822ddf56
Rembrant Pasardan <160525899+BrantySis@users.noreply.github.com>
2024-10-15 22:27:49 +0800

    Activity 5 adding controller and routing

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
-    //
+    public function welcome()
+    {
+        $todos = Todo::all();
+       //dd($todos);
+       return view('welcome', [
+        'todos' => $todos,
+    ]);
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+        
+    public function create()
+    {
+        return view('create');
+    }
 }
