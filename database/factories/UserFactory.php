952dd5ce280cc51fcf701e01333d25f6d210c072
Dwargom12 <derickaton12345@gmail.com>
2024-10-15 16:24:57 +0800

    Acitivity 4

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,21 +1,15 @@
+
 <?php
 
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class UserFactory extends Factory
+class todofactoryFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
@@ -24,21 +18,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
-    }
+            //'name' => fake()->name(),
+            //'email' => fake()->unique()->safeEmail(),
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'is_completed' => fake()->boolean(),
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        ];
     }
 }
