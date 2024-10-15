4cc5e9346b4756b9978764095190c7fbf06438df
Megan Ys B. Tepait <meganclan2017@gmail.com>
2024-10-15 11:56:01 +0800

    ACTIVITY 6 - New Layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,8 @@
+ <!-- Footer -->
+ <footer class="bg-white dark:bg-gray-800 shadow mt-auto">
+        <div class="container mx-auto px-6 py-4">
+            <p class="text-center text-gray-600 dark:text-gray-400">
+                &copy; 2024 Todo App. All rights reserved.
+            </p>
+        </div>
+    </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,5 @@
 <x-layout>
-    <x-slot:heading>
-        Welcome
-    </x-slot>
- <h1>Welcome Page</h1>
- 
-
- <table class="table-auto">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Title</th>
-      <th>Description</th>
-      <th>Completed</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach($todos as $todo)
-    <tr>
-      <td>{{ $todo->id }}</td>
-      <td>{{ $todo->title }}</td>
-      <td>{{ $todo->description }}</td>
-      <td>{{ $todo->completed ? 'Yes' : 'No' }}</td>
-    </tr>
-   @endforeach
-  </tbody>
-</table>
+    <div class="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
+        <h1 class="text-2xl font-semibold mb-4">Welcome Page</h1>
+    </div>
 </x-layout>
