6ac9a7caeca3cecc9afb148614a749cf0d784c8b
DESKTOP-E49EA52\JMC <jancabahug31@gmail.com>
2024-10-16 14:47:14 +0800

    activity 1 done

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="en" class="h-full bg-gray-100">
 
 <head>
+    <!-- activity 1 -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HakunaMatata</title>
