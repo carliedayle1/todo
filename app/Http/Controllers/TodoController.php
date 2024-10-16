45acaf14e857376052325ef344ec2094c487d8d3
ninagene <genekidanin@gmail.com>
2024-10-15 19:24:35 -0700

    Activity 5 - Display Todo with Tables

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,18 +1,28 @@
 <?php
+
 namespace App\Http\Controllers;
+
+use App\Models\Todo;
 use Illuminate\Http\Request;
+
 class TodoController extends Controller
 {
     public function about(){
         return view('about');
     }
+
     public function welcome(){
-        $name = "Hello World!";
+        
+        $todos = Todo::all();
+
+        //dd($todos);
         return view('welcome', [
-            'name' => $name,
+            'todos' => $todos,
         ]);
     }
+
     public function contact(){
         return view('contact');
     }
+
 }
