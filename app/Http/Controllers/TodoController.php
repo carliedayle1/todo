139ae39c143886329f8d61461099e1633aeea20a
ninagene <genekidanin@gmail.com>
2024-10-15 19:05:40 -0700

    Activity - 3 Controllers

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+class TodoController extends Controller
+{
+    public function about(){
+        return view('about');
+    }
+    public function welcome(){
+        $name = "Hello World!";
+        return view('welcome', [
+            'name' => $name,
+        ]);
+    }
+    public function contact(){
+        return view('contact');
+    }
+}
