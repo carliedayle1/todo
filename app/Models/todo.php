87ef6e072c27d61eac07ae560c2519895d82e83e
unknown <Admin@CSLAB218.CSSERVER.local>
2024-10-21 14:34:14 +0800

    activity 5 - display todos

--- app/Models/todo.php
+++ app/Models/todo.php
@@ -5,10 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class todo extends Model
+class Todo extends Model
 {
     use HasFactory;
-
-    protected $table = 'todos';
-    protected $fillable = ['title', 'description', 'completed'];
 }
