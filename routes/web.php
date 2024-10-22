d890cb4c6bfc37c5ee912ede3ed3628c319e839c
Jazzy <jazrylpadin@gmail.com>
2024-10-23 03:11:02 +0800

    Activity 3 - Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [TodoController::class, 'welcome']);
 
 Route::get('/about', function () {
     return view('about');
 });
+Route::get('/about', [TodoController::class, 'about']);
 
 Route::get('/contacts', function () {
     return view('contacts');
 });
+Route::get('/contacts', [TodoController::class, 'contacts']);
