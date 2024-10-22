d8238f659de98a41282d89c32845c9d2c2bcf8f5
Jazzy <jazrylpadin@gmail.com>
2024-10-23 03:49:01 +0800

    Activity 5 - Display Todo with Tables

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Http\Controllers;
+use App\Models\Todo;
 use Illuminate\Http\Request;
 class TodoController extends Controller
 {
@@ -8,8 +9,12 @@ class TodoController extends Controller
     }
     public function welcome(){
         $name = "Hello World!";
+        
+        $todos = Todo::all();
+        //dd($todos);
         return view('welcome', [
             'name' => $name,
+            'todos' => $todos,
         ]);
     }
     public function contact(){
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,25 @@
-<x-layout>
-    <x-slot:heading>
-        Welcome
+Welcome
     </x-slot>
  <h1>Welcome Page</h1>
+ 
+ <table class="table-auto">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Title</th>
+      <th>Description</th>
+      <th>Completed</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($todos as $todo)
+    <tr>
+      <td>{{ $todo->id }}</td>
+      <td>{{ $todo->title }}</td>
+      <td>{{ $todo->description }}</td>
+      <td>{{ $todo->completed ? 'Yes' : 'No' }}</td>
+    </tr>
+   @endforeach
+  </tbody>
+</table>
 </x-layout>
