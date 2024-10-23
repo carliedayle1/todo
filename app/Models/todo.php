e9ada9ce91f0dd0d80581edc73c6f153aae57573
CarlaJay <carlajaygersamio081@gmail.com>
2024-10-23 18:23:22 +0800

    Activity 2 - Layout

--- app/Models/todo.php
+++ app/Models/todo.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Todo extends Model
+class Todo extends 
 
 {
-    use HasFactory; 
-}
+    use HasFactory;
+} 
