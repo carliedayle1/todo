633b5812dd16dcfb70731136848961317be17c75
jade <147321226+mmrnljd@users.noreply.github.com>
2024-10-23 22:18:23 +0800

    activity 8 - CRUD

--- app/Http/Controllers/todocontroller.php
+++ app/Http/Controllers/todocontroller.php
@@ -2,27 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Todo;
+use Illuminate\Http\Request;
 
-class TodoController extends Controller
+class todocontroller extends Controller
 {
-    public function about(){
-        return view('about');
+    public function about()
+    {
+       return view('about');
     }
-    
-    public function home(){
+
+    public function home()
+    {
         $todos = Todo::all();
+
         
-        return view('welcome', [
+        return view('home', [
             'todos' => $todos,
         ]);
     }
     
-    public function contact(){
+    public function contact()
+    {
         return view('contact');
     }
-    public function create(){
-        return view('create');
+    public function createtodo()
+    {
+        return view('createtodo');
     }
 }
+    public function store()
+    {
+ 
+        Todo::create([
+            'title' => request()->title,
+            'description' => request()->description,
+            'completed' => request()->completed == 'Yes' ? true : false
+        ]);
+    return redirect('/');
+    }
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return back();
+    }
+    public function edit(Todo $todo){
+        //dd($todo);
+        return view('edit',[
+            'todo' => $todo
+        ]);
+    }
+    public function update(Todo $todo)
+    {
+        //dd(request()->all());
+        $todo->update([
+            'title' => request()->title,
+            'description' => request()->description,
+            'completed' => request()->completed == 'Yes' ? true : false
+        ]);
+        return redirect('/');
+    }
