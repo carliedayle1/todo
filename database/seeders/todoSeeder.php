05eb949c0977c85ab606f942863af10ae78eaa98
jade <147321226+mmrnljd@users.noreply.github.com>
2024-10-23 23:37:26 +0800

    activity 10 - eloquent relationships

--- database/seeders/todoSeeder.php
+++ database/seeders/todoSeeder.php
@@ -4,14 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Todo;
+use App\Models\Comment;
 
-class todoSeeder extends Seeder
+class TodoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        todo::factory()->count(10)->create();
+        Todo::factory(10)
+        ->has(Comment::factory()->count(3))
+        ->create();
     }
 }
