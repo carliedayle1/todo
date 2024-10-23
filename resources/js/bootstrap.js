29bee7acb3d2f78aacb5e92263fbaee6bf99a1bd
axell27 <sirjhonny147@gmail.com>
2024-10-24 04:32:32 +0800

    activity 6

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,7 +2,6 @@ import axios from "axios";
 window.axios = axios;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
 // This code should be added to <head>.
 // It's used to prevent page load glitches.
 const html = document.querySelector("html");
@@ -14,7 +13,6 @@ const isDarkOrAuto =
     localStorage.getItem("hs_theme") === "dark" ||
     (localStorage.getItem("hs_theme") === "auto" &&
         window.matchMedia("(prefers-color-scheme: dark)").matches);
-
 if (isLightOrAuto && html.classList.contains("dark"))
     html.classList.remove("dark");
 else if (isDarkOrAuto && html.classList.contains("light"))
