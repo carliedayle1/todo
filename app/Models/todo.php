5d2ea71eb5b868615bb0449a0bcfb2c1b17ca6af
CarlaJay <carlajaygersamio081@gmail.com>
2024-10-23 20:30:56 +0800

    Activity 5 - Display Todos

--- app/Models/todo.php
+++ app/Models/todo.php
@@ -5,8 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Todo extends 
+class todo extends 
 
 {
     use HasFactory;
+
+    //protected $table = 'todos';
+    //protected $fillable = ['title', 'description', 'completed'];
 } 
