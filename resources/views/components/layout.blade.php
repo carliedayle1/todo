927bf91fb552792da535ae5af8db73a71e467913
axell27 <sirjhonny147@gmail.com>
2024-10-24 04:38:26 +0800

    activity 6

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200 min-h-screen flex flex-col">
+<body class="bg-gray-100 dark:bg-gray-900 text-gray-850 dark:text-gray-200 min-h-screen flex flex-col">
     <!-- Header -->
     <x-navbar />
 
