e8c6c1888855f5a98be0fbabbe15b65250daeb1b
jade <147321226+mmrnljd@users.noreply.github.com>
2024-10-23 14:44:12 +0800

    activity 7 - forms

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
+use App\Http\Controllers\TodoController; //remove
 
 Route::get('/', [TodoController::class, 'home']);
 
-Route::get('about', [TodoController::class, 'about']);
+Route::get('/about', [TodoController::class, 'about']);
 
-Route::get('contact', [TodoController::class, 'contact']);
+Route::get('/contact', [TodoController::class, 'contact']);
 
-Route::get('create', [TodoController::class, 'create']);
+Route::get('/createtodo', [TodoController::class, 'createtodo']);
+//Route::get('/', function () {
+    //return view('welcome');
+//});
+
+//Route::get('/about', function () {
+    //return view('about');
+//});
+
+//Route::get('/contact', function () {
+    //return view('contact');
+//});
