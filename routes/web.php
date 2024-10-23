3116967bd76ed812859903189020c09dd6ef3409
axell27 <sirjhonny147@gmail.com>
2024-10-24 04:56:12 +0800

    activity 5-7

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home route
+Route::get('/', [TodoController::class, 'welcome'])->name('welcome');
 
-Route::get('/about', function () {
-    return view('about');
-});
+// About page
+Route::get('/about', [TodoController::class, 'about']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+// Contact page
+Route::get('/contact', [TodoController::class, 'contact']);
+
+// Create Todo routes
+Route::get('/create', [TodoController::class, 'create'])->name('todo.create');
+Route::post('/store', [TodoController::class, 'store'])->name('todo.store');
+
+// Edit Todo routes
+Route::get('/todos/{id}/edit', [TodoController::class, 'edit'])->name('todo.edit');
+Route::put('/todos/{id}', [TodoController::class, 'update'])->name('todo.update');
+
+// Delete Todo route
+Route::delete('/todos/{id}', [TodoController::class, 'delete'])->name('todos.delete');
