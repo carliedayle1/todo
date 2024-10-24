527c5debca73a14f119404ef5fb693a35f40b6cd
Aj Villamor <ajvillamorml@gmail.com>
2024-10-24 15:41:45 +0800

    Activity 3 - Controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function home(){
+        
+        $todos = Todo::all();
+       return view('home', ['todos' => $todos]);
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+    
 }
