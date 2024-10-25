0b8bc66a8b65777d9b985b411a0203f0b96f3ec5
Paricakes <icedc156@gmail.com>
2024-10-25 22:01:30 +0800

    activity 5 - display todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,17 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Todo;
 
 class TodoController extends Controller
 {
     public function about(){
         return view('about');
     }
-
+    
     public function home(){
-        return view('welcome');
+        $todos = Todo::all();
+        
+        return view('welcome', [
+            'todos' => $todos,
+        ]);
     }
-
+    
     public function contact(){
         return view('contact');
     }
