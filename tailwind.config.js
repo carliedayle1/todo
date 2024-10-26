40caf36f97da25e342476a02ab7176db6a859613
mymac <mymac@MacBook-Air-de-mymac.local>
2024-10-26 22:36:59 +0800

    Activity 6 - New Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-        "node_modules/preline/dist/*.js",
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      'node_modules/preline/dist/*.js',
     ],
     theme: {
-        extend: {},
+      extend: {},
     },
-    plugins: [require("preline/plugin"), require("@tailwindcss/forms")],
+    plugins: [
+      require('preline/plugin'),
+    ],
     darkMode: "class",
-};
+  }
+  
