5871f110445a1791fd57a71d7895e3485000672a
Nykrow7 <127639817+Nykrow7@users.noreply.github.com>
2024-10-26 15:15:59 +0800

    Activity 8 - CRUD

--- app/Models/todo.php
+++ app/Models/todo.php
@@ -8,7 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class todo extends Model
 {
     use HasFactory;
-
-    protected $table = 'todos';
     protected $fillable = ['title', 'description', 'completed'];
 }
