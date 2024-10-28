79f422338e952ec3d9b482ce32df1a5fdba544dc
RielMaico <rielmaicoaparre1@gmail.com>
2024-10-28 16:29:12 +0800

    Activity 2 Migration and models

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+
     use HasFactory;
+    protected $guarded = [];
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
