641f2b0b30a3651d55fbf63bec6ed7f82a97c495
RielMaico <rielmaicoaparre1@gmail.com>
2024-10-28 16:23:32 +0800

    Activity 1 - Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
     <div class="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
+      
         <h1 class="text-2xl font-semibold mb-4">Create Todo</h1>
 
         <div class="container p-4">
