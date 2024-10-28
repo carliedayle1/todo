bdb881e3a8359df21f6ed57ca6e58b9ba9fdaada
JethroDungog <dungogjethro@gmail.com>
2024-10-28 15:43:29 +0800

    Activity 2 Migration and models

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
