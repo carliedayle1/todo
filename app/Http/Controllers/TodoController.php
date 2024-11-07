4298e1b0ed4777e91b369a21dae0b24ce12845c4
christy-mae <christy221mae@gmail.com>
2024-11-07 17:18:05 +0800

    Activity 8 - CRUD(revised)

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -10,20 +10,50 @@ class TodoController extends Controller
     public function about(){
         return view('about');
     }
+
     public function welcome(){
         
-        $todos = Todo::all();
+        $todos = Todo::latest()->get();
 
         //dd($todos);
         return view('welcome', [
             'todos' => $todos,
         ]);
     }
+
     public function contact(){
         return view('contact');
     }
 
     public function create(){
         return view('create');
+    }
+
+    public function store(){
+        Todo::create([
+            'title' => request()->title,
+            'description' => request()->description,
+            'completed' => request()->completed == 'Yes' ? true : false
+        ]);
+        return redirect('/');
+    }  
+
+    public function destroy(Todo $todo){
+        
+        $todo->delete();
+        return back();
+    }
+    public function edit(Todo $todo){
+        return view('edit', [
+            'todo' => $todo
+        ]);
+    }
+    public function update(Todo $todo){
+        $todo->update([
+            'title' => request()->title,
+            'description' => request()->description,
+            'completed' => request()->completed == 'Yes' ? true : false
+        ]);
+        return redirect('/');
     }
 }
